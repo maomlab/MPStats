d032c649a7820da5f8d218db455f8efeb61e54da
Ubuntu <ubuntu@superior.sextonlab.com>
2020-03-01 02:20:13 +0000

    Update documentation for Zprime_by_dye plot

--- R/plot_Zprime_by_dye.R
+++ R/plot_Zprime_by_dye.R
@@ -1,12 +1,46 @@
 #' Plot Zprime score by Dye combination
 #'
-#' @param Zprime_by_plate tibble::tibble
+#'    This makes a plot like from the UpSetR package with two sections
+#'    The upper section is a dot plot with the Zprime score on the
+#'    y-axis and the dye set on the x-axis. The value for each dye set
+#'    is represented by a dot at the mean for each plate and error
+#'    bars as the standard error in the mean. The lower section
+#'    indicates which dyes are part of the dye_set. The y-axis give
+#'    each dye and and the x-axis is the dye_set lining up with the
+#'    dye_set on the upper section The values are represented as dots
+#'    for each dye that is in the dye set and a line from the first to
+#'    the last dye in the set. Columns across both sections alternate
+#'    and the rows in the bottom section alternate shading.
+#'
+#' @param Zprime_by_plate tibble::tibble with columns
+#'        dye_set  : factor this should be in the order that they will be displayed
+#'                   the name of the dye set should be a string of the individual dyes
+#'                   separated by the <dye_set_separator>.
+#'        Zprime   : Score to be plotted should be in (-Inf, 1) where
+#'                   1 is perfect, .5-1 is good, 0-0.5 is poor, and < 1 is bad
+#'        plate_id : Identifiers for different plates. The Zscores should be computed
+#'                   separately for each plate and then these Zprime scores will be
+#'                   aggregated to make the plot
+#' @param dye_set_separator separator to figure out what dyes make up the dye_set variable.
+#'        Default so "_"
 #' @param subtitle string subtitle for plot
 #' @return ggplot2 plot
+#'
+#' dye_plot <- MPStats::plot_Zprime_by_dye(Zprime_by_plate)
+#' ggplot2::ggsave(
+#'   plot=dye_plot,
+#'   filename=paste0("product/Zprime_by_dye_", MPStats::date_code(), ".pdf"),
+#'   width=4, height=6,
+#'   useDingbats=FALSE)
+#' ggplot2::ggsave(
+#'   plot=dye_plot,
+#'   filename=paste0("product/Zprime_by_dye_", MPStats::date_code(), ".png"),
+#'   width=4, height=6)
 #'
 #' @export
 plot_Zprime_by_dye <- function(
   Zprime_by_plate,
+  dye_set_separator="_",
   subtitle=NULL){
 
   Zprime_summary <- Zprime_by_plate %>%
@@ -15,16 +49,18 @@ plot_Zprime_by_dye <- function(
       mean = mean(Zprime),
       n_plates = dplyr::n(),
       std_err = sd(Zprime) / sqrt(dplyr::n())) %>%
-    dplyr::mutate(set_size = dye_set %>% stringr::str_count("_") + 1) %>%
-    dplyr::arrange(set_size, dye_set) %>%
+    dplyr::mutate(set_size = dye_set %>% stringr::str_count(dye_set_separator) + 1) %>%
     dplyr::select(-set_size) %>%
     dplyr::mutate(set_id = dplyr::row_number())
 
+  Zprime_limits <- c(
+    min(Zprime_by_plate %>% magrittr::extract2("Zprime") %>% min(), 0) - 0.05,
+    1)
 
   sets_1hot <- Zprime_summary %>%
     dplyr::select(dye_set, set_id) %>%
     dplyr::mutate(count=1) %>%
-    tidyr::separate_rows(dye_set, sep="_") %>%
+    tidyr::separate_rows(dye_set, sep=dye_set_separator) %>%
     tidyr::pivot_wider(id_cols=set_id, names_from=dye_set, values_from=count) %>%
     tibble::column_to_rownames("set_id") %>%
     t
@@ -52,10 +88,6 @@ plot_Zprime_by_dye <- function(
         value > 0L,
         paste0(x, "yes"),
         paste0(row_number(), "no")))
-
-  Zprime_limits <- c(
-    min(Zprime_by_plate %>% magrittr::extract2("Zprime") %>% min(), 0) - 0.05,
-    1)  
 
   upper_sets_shading_data <- sets_data %>%
     dplyr::distinct(x) %>%
@@ -86,7 +118,7 @@ plot_Zprime_by_dye <- function(
       ymin=y-0.5,
       ymax=y+0.5,
       color=shade_color)
-        
+
   dots_plot <- ggplot() +
     theme(
       legend.position = c(0.5, 0.9),
@@ -121,7 +153,7 @@ plot_Zprime_by_dye <- function(
       fill = shade_color,
       alpha = shade_alpha) +
     geom_hline(yintercept=0.5, size=0.5) +
-    geom_hline(yintercept=0.0, size=0.5) +      
+    geom_hline(yintercept=0.0, size=0.5) +
     geom_errorbar(
       data=Zprime_summary,
       aes(x=set_id, ymin=mean-std_err, ymax=mean+std_err)) +
