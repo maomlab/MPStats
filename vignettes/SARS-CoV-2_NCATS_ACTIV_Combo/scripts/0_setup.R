e3b5d6c10fddd489ef9ff9b9f63690383afe9348
Matthew O'Meara <maom@gl-login2.arc-ts.umich.edu>
2020-11-19 15:01:18 -0500

    initial use of batchtools

--- vignettes/SARS-CoV-2_NCATS_ACTIV_Combo/scripts/0_setup.R
+++ vignettes/SARS-CoV-2_NCATS_ACTIV_Combo/scripts/0_setup.R
@@ -3,8 +3,24 @@
 library(batchtools)
 
 
+if (!dir.exists(paths="/scratch/maom_root/maom99/maom/SARS-CoV-2_iAEC2_Combo")) {
+    cat("Creating work direcotry '/scratch/maom_root/maom99/maom/SARS-CoV-2_iAEC2_Combo'\n")
+    dir.create("/scratch/maom_root/maom99/maom/SARS-CoV-2_iAEC2_Combo")
+}
 
-batchtools::makeRegistry(
+batchtools_registry <- batchtools::makeRegistry(
     file.dir = "intermediate_data/batchtools_registry",
-    work.dir = "/mnt/scratch/maom99_root/maom/SARS-CoV-2_iAEC2_Combo")
+    work.dir = "/scratch/maom_root/maom99/maom/SARS-CoV-2_iAEC2_Combo",
+    seed = 22336)
 
+# https://mllg.github.io/batchtools/reference/makeClusterFunctionsSlurm
+batchtools_registry$cluster.functions = batchtools::makeClusterFunctionsSlurm(
+  template = "scripts/batchtools_slurm-greatlakes.tmpl",
+  array.jobs = TRUE,
+  nodename = "localhost",
+  scheduler.latency = 1,
+  fs.latency = 65)
+
+
+batchtools::saveRegistry(
+    reg = batchtools_registry)
